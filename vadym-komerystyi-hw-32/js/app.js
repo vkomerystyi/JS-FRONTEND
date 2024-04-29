628e7252a2c8f671c3760881927c3007d9231c06
v.komerystyi <v.komerystyi@gmail.com>
2024-04-29 13:38:47 +0300

    change JS file home work 32

--- vadym-komerystyi-hw-32/js/app.js
+++ vadym-komerystyi-hw-32/js/app.js
@@ -17,14 +17,10 @@ function handleForm(event) {
 }
 
 function getUserAge(value) {
-  const currentTime = new Date()
-  let currentDay = currentTime.getDate()
-  let currentMonth = currentTime.getMonth() + 1
-  let currentFullYear = currentTime.getFullYear()
-
-  const inputDay = parseInt(value.day)
   const inputMonth = parseInt(value.month)
+  const inputDay = parseInt(value.day)
   const inputYear = parseInt(value.year)
+  const currentFullYear = new Date().getFullYear()
 
   for (let key in value) {
     const field = document.querySelector(`.form__item input[name='${key}']`)
@@ -37,7 +33,21 @@ function getUserAge(value) {
       if (!errorText) {
         const errorElement = document.createElement('span')
         errorElement.classList.add('error-text')
-        errorElement.innerText = `Must be a valid ${key}`
+        errorElement.innerText = `Please enter ${key}`
+        formItem.appendChild(errorElement)
+      }
+    } else if (
+      (key === 'month' && (inputMonth < 1 || inputMonth > 12)) ||
+      (key === 'day' && (inputDay < 1 || inputDay > 31)) ||
+      (key === 'year' && inputYear > currentFullYear) ||
+      inputYear < 0
+    ) {
+      field.classList.add('error')
+      formItem.classList.add('error')
+      if (!errorText) {
+        const errorElement = document.createElement('span')
+        errorElement.classList.add('error-text')
+        errorElement.innerText = `Invalid ${key}`
         formItem.appendChild(errorElement)
       }
     } else {
@@ -46,41 +56,38 @@ function getUserAge(value) {
       if (errorText) {
         errorText.remove()
       }
-    }
-
-    if (
-      isNaN(inputDay) ||
-      isNaN(inputMonth) ||
-      isNaN(inputYear) ||
-      inputMonth < 1 ||
-      inputMonth > 12 ||
-      inputYear > currentFullYear ||
-      (inputYear === currentFullYear && inputMonth > currentMonth) ||
-      (inputYear === currentFullYear && inputMonth === currentMonth && inputDay > currentDay)
-    ) {
-      console.log('Invalid date')
-    } else {
-      let days = currentDay - inputDay
-      let months = currentMonth - inputMonth
-      let years = currentFullYear - inputYear
-
-      // Якщо отримали від'ємне значення днів, потрібно вирахувати це з місяців
-      if (days < 0) {
-        months--
-        // Визначаємо кількість днів у попередньому місяці
-        const prevMonthDays = new Date(currentFullYear, currentMonth - 1, 0).getDate()
-        days += prevMonthDays
-      }
-
-      // Якщо отримали від'ємне значення місяців, потрібно вирахувати це з років
-      if (months < 0) {
-        years--
-        months += 12 // Додаємо 12 місяців до від'ємного значення
-      }
-      valueYears.innerText = years
-      valueMonths.innerText = months
-      valueDays.innerText = days
+      ageCount(value)
     }
   }
 }
+
+function ageCount(value) {
+  const currentTime = new Date()
+  let currentDay = currentTime.getDate()
+  let currentMonth = currentTime.getMonth() + 1
+  let currentFullYear = currentTime.getFullYear()
+
+  const inputDay = parseInt(value.day)
+  const inputMonth = parseInt(value.month)
+  const inputYear = parseInt(value.year)
+
+  let days = currentDay - inputDay
+  let months = currentMonth - inputMonth
+  let years = currentFullYear - inputYear
+
+  if (days < 0) {
+    months--
+    const prevMonthDays = new Date(currentFullYear, currentMonth - 1, 0).getDate()
+    days += prevMonthDays
+  }
+
+  if (months < 0) {
+    years--
+    months += 12
+  }
+  valueYears.innerText = years
+  valueMonths.innerText = months
+  valueDays.innerText = days
+}
+
 form.addEventListener('submit', handleForm)
