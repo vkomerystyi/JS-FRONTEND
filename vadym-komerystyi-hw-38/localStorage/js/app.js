3cdbfbdecf9c065035bc2ca0d406f107a573198c
v.komerystyi <v.komerystyi@gmail.com>
2024-06-04 13:07:51 +0300

    completed localStorage

--- vadym-komerystyi-hw-38/localStorage/js/app.js
+++ vadym-komerystyi-hw-38/localStorage/js/app.js
@@ -1,60 +1,53 @@
-//  get and set localStorage data
+// Utility functions for localStorage
 const getLocalStorage = () => JSON.parse(localStorage.getItem('data')) ?? []
 const setLocalStorage = (dbText) => localStorage.setItem('data', JSON.stringify(dbText))
-// create
+// Create
 const createData = (dataText) => {
   const dbText = getLocalStorage()
   dbText.push(dataText)
   setLocalStorage(dbText)
 }
-// read
-const readData = () => {
-  return getLocalStorage()
-}
-// update
+// Read
+const readData = () => getLocalStorage()
+// Update
 const updateData = (index, data) => {
   const dbText = readData()
   dbText[index] = data
   setLocalStorage(dbText)
 }
-// delete
+// Delete
 const deleteData = (index) => {
   const dbText = readData()
   dbText.splice(index, 1)
   setLocalStorage(dbText)
 }
-
-const isValid = () => {
-  return document.getElementById('textarea').reportValidity()
-}
-
+// Validate input
+const isValid = () => document.getElementById('textarea').reportValidity()
+// Save data (create or update)
 const saveData = () => {
-  if (isValid()) {
-    const dataToDo = {
-      text: document.getElementById('textarea').value,
-    }
-    const index = textarea.dataset.index
-    if (index === '') {
-      createData(dataToDo)
-    } else {
-      updateData(Number(index), dataToDo)
-    }
-
-    textarea.value = ''
-    textarea.dataset.index = ''
-    updateList()
+  if (!isValid()) {
+    return
   }
-}
+  const textarea = document.getElementById('textarea')
+  const dataToDo = { text: textarea.value }
 
+  const index = textarea.dataset?.index ? Number(textarea.dataset.index) : null
+  const createOrUpdate = index === null ? createData : updateData.bind(null, index)
+  createOrUpdate(dataToDo)
+
+  textarea.value = ''
+  textarea.dataset.index = ''
+
+  updateList()
+}
 const createList = (data, index) => {
   const newLi = document.createElement('li')
   newLi.classList.add('list-group-item')
   newLi.innerText = `${data.text}`
-  newLi.id = `item-${index}`
 
   newLi.addEventListener('click', () => {
-    const newText = document.getElementById('textarea')
-    newText.value = data.text
+    const textarea = document.getElementById('textarea')
+    textarea.value = data.text
     textarea.dataset.index = index
   })
 
@@ -63,20 +56,20 @@ const createList = (data, index) => {
     deleteData(index)
     updateList()
   })
-
   document.querySelector('.list-group').appendChild(newLi)
 }
-
+// Clear list
 const clearList = () => {
-  const newLi = document.querySelectorAll('li')
-  newLi.forEach((li) => li.parentNode.removeChild(li))
+  const listItems = document.querySelectorAll('li')
+  listItems.forEach((li) => li.parentNode.removeChild(li))
 }
-
+// Update list
 const updateList = () => {
   const dbText = readData()
   clearList()
   dbText.forEach(createList)
 }
-
+// Initial update of the list
 updateList()
+// Add event listener for save button
 document.getElementById('save').addEventListener('click', saveData)
